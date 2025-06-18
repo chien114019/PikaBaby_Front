8b11fd6eaeadc0858b68e2a58d136f418fde508b
chen-yu-chien <vanilla30362@gmail.com>
2025-06-18 15:37:09 +0800

    20250618 新增前台訂單查詢功能

--- BabyBack/src/main/java/com/example/demo/controller/SalesOrderController.java
+++ BabyBack/src/main/java/com/example/demo/controller/SalesOrderController.java
@@ -3,14 +3,22 @@ package com.example.demo.controller;
 import com.example.demo.model.*;
 import com.example.demo.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@CrossOrigin(origins = "*")
 @Controller
 @RequestMapping("/orders")
 public class SalesOrderController {
@@ -97,5 +105,12 @@ public class SalesOrderController {
         return "redirect:/orders";
     }
 
+//   ========== 前台API============
+    @GetMapping("/search/cust/{custId}")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> getOrdersByCustId(@PathVariable String custId) {
+    	return ResponseEntity.ok(orderService.getOrdersByCustId(custId));
+    }
+    
 
 }

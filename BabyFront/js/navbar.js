53353cf6201b18351787ef8c6e6afe484c3a0cb2
chen-yu-chien <vanilla30362@gmail.com>
2025-06-18 09:35:28 +0800

    20250628 新增前台優惠倒數功能

--- BabyFront/js/navbar.js
+++ BabyFront/js/navbar.js
@@ -38,3 +38,30 @@ document.addEventListener("scroll", function () {
         $(".navbar img#logo").css("width", "0px");
     }
 });
+
+let spans = $(".top-banner span")
+setInterval(function () {  // 設置倒數計時: 結束時間 - 當前時間
+
+    // 當前時間
+    var time = new Date();
+    var nowTime = time.getTime(); // 獲取當前毫秒數
+    // 設置結束時間 : 5月13號 15點0分0秒
+    time.setMonth(6); //   獲取當前 月份 (從 '0' 開始算)
+    time.setDate(15); //   獲取當前 日
+    time.setHours(15); //   獲取當前 時
+    time.setMinutes(0); //   獲取當前 分
+    time.setSeconds(0);
+    var endTime = time.getTime();
+
+    // 倒數計時: 差值
+    var offsetTime = (endTime - nowTime) / 1000; // ** 以秒為單位
+    var sec = parseInt(offsetTime % 60); // 秒
+    var min = parseInt((offsetTime / 60) % 60); // 分 ex: 90秒
+    var hr = parseInt((offsetTime / 60 / 60) % 60); // 時
+    var day = parseInt(offsetTime / 60 / 60 / 24);
+
+    spans[0].textContent = day;
+    spans[1].textContent = hr;
+    spans[2].textContent = min;
+    spans[3].textContent = sec;
+}, 1000);

47b06ce2eb2a4938b974e951b506a67be0d5d6e2
chen-yu-chien <vanilla30362@gmail.com>
2025-07-06 23:24:06 +0800

    20250706 修改為前端repo

--- BabyBack/src/main/java/com/example/demo/repository/Company2.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Company;
-
-public interface Company2 {
-    Company getCompanyInfo();
-    Company getById(Long id);
-    void saveOrUpdate(Company company);
-}
